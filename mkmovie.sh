f5c8277a3929ce7361792733df3305093859c2eb
t2hsu <t2hsu@aha.ucsd.edu>
2024-04-18 15:18:04 -0700

    Add a movie code

---
+++ mkmovie.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#ffmpeg -framerate 30 -pattern_type glob -i '*.png' \
+#  -c:v libx264 -pix_fmt yuv420p out.mp4
+
+#ffmpeg -f concat -i file_list -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p animation_Lx100_U20_dT100_MYNN25.mp4
+ffmpeg \
+    -r 1/5 \
+    -pattern_type glob \
+    -i 'figures/snapshots_15min_new/Lx100_U20_dT100_MYNN25/snapshot_????-????.png' \
+    -c:v libx264       \
+    -pix_fmt yuv420p   \
+    animation_Lx100_U20_dT100_MYNN25.mp4
