21d3ae4a59f2aa6c29a2cc2f0b0943a968fb3747
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2025-10-28 12:39:20 -0700

    Update file

--- 11_plot_ocean_SST_analysis.sh
+++ 11_plot_ocean_SST_analysis.sh
@@ -3,7 +3,7 @@
 source 00_setup.sh
 
 python3 $src_dir/plot_SST_map_and_analysis.py \
-    --input-file $data_dir/hycom_glby_930_2024010112_t000_t.nc \
+    --input-file $data_dir/hycom_glby_930_2024010112_t000_t_depth5.nc \
     --lat-rng 25 50 \
     --lon-rng 160 200 \
     --cutoff-wvlen 1000.0 \
